722137256146319d14aee344d17b424b9ff9a5fd
Burak Özkan <burak.ozkan@path.com.tr>
2021-08-25 00:37:21 +0300

    connect db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,15 @@
 
-console.log('Selam !')
+import { MikroORM } from "@mikro-orm/core";
+import {Post} from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+
+  const post = orm.em.create(Post, {title: 'my first post'});
+  await orm.em.persistAndFlush(post);
+  console.log('---------- sql 2 ---------')
+  await orm.em.nativeInsert(Post, {title: 'my first post 2'})
+}
+
+main();
