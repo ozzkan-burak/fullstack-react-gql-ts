c67beb02604cf6f497ed9d7c57aba77f1dfdb135
Burak Özkan <burak.ozkan@path.com.tr>
2021-09-01 00:51:43 +0300

    add express and apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,43 @@
 
 import { MikroORM } from "@mikro-orm/core";
-import {Post} from "./entities/Post";
+import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
+
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
   const post = orm.em.create(Post, {name: 'my first post'});
   await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
+  const app = express();
+  const PORT = 4000;
 
-  console.log('POSTLAR', posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false
+    }),
+    context: ()=> ({em: orm.em})
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app })
+
+  app.get('/', (_req, res) => {
+    res.send('ANSAYFA')
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server run on ${PORT} `)
+  })
 }
 
-main();
+main().catch((err) => {
+  console.error(err);
+});
